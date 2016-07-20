7f14e0ff0ca0f38d437c78af2ba93f9e85e822de
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-07-20 23:07:05 +0900

    Install trained data by downloading it!

---
+++ jsk_pr2_wandering/scripts/install_trained_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+
+from jsk_data import download_data
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', dest='quiet', action='store_false')
+    args = parser.parse_args()
+    quiet = args.quiet
+
+    PKG = 'jsk_pr2_wandering'
+
+    download_data(
+        pkg_name=PKG,
+        path='trained_data/fcn32s_2000_2016-07-20-22-52-51.chainermodel',
+        url='https://drive.google.com/uc?id=0B9P1L--7Wd2vQWV0QUxiZ3BhSUE',
+        md5='1e7a815f4ba8e0747340f9861d59b06e',
+        quiet=quiet,
+    )
+
+
+if __name__ == '__main__':
+    main()
